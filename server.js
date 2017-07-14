a62e4af2aecdab1e9b17bf05b80b36748bcedc22
davefelcey <davefelcey@gmail.com>
2017-07-14 16:59:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-// routes will go here
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/', function(req, res) {
+    response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    res.send(JSON.stringify({ "speech": response, "displayText": response 
+      //  "speech" is the spoken version of the response, "displayText" is the visual version
+  }));
 
 // start the server
 app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+console.log('Server started on port: ' + port);
